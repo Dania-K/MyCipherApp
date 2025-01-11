23be52fa62ea74f5f0e1dd27c138e6727fb51750
Dania-K <daniakedir2020@gmail.com>
2025-01-10 18:57:54 -0800

    Update CiperApp.py

--- CiperApp.py
+++ CiperApp.py
@@ -37,7 +37,7 @@ if st.button("Cipher"):
 
 if st.button("Decipher"):
     if text:
-        decrypted_text = caesar_encrypt(text, -shift)  # Use negative shift for decryption
+        decrypted_text = caesar(text, -shift)  # Use negative shift for decryption
         st.write('**Input text:**', text)
         st.write('**Decrypted text:**', decrypted_text)
     else:
