c662e00608257b502a6c738adeb2d02b4c09d038
Dania-K <daniakedir2020@gmail.com>
2025-01-11 10:08:24 -0800

    Update CiperApp.py

--- CiperApp.py
+++ CiperApp.py
@@ -27,7 +27,7 @@ text = st.text_input("Enter the text to be encrypted:")
 shift = st.number_input("Enter the shift value (1-25):", min_value=1, max_value=25, value=5)
 
 # Button to encrypt
-if st.button("Encode"):
+if st.button("Encrypt Now"):
     if text:
         encrypted_text = caesar(text, shift)
         st.write('**Plain text:**', text)
@@ -35,7 +35,7 @@ if st.button("Encode"):
     else:
         st.warning("Please enter a text to encrypt.")
 
-if st.button("Decode"):
+if st.button("Decrypt Now"):
     if text:
         decrypted_text = caesar(text, -shift)  # Use negative shift for decryption
         st.write('**Input text:**', text)
